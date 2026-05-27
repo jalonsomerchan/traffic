5c84307f954c076ab59677017c224c6f817ad1ac
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-27 12:55:03 +0200

    Add editable simulation timing config

---
+++ js/SimulationConfig.js
@@ -0,0 +1,9 @@
+export const SIMULATION_CONFIG = {
+  roadWorks: {
+    constructionDurationSeconds: 20,
+    repairHealthPerSecond: 4,
+    upgradeDurationSeconds: 34,
+    upgradeHealthPerSecond: 2,
+    activeUpgradeMonthlyCostMultiplier: 28,
+  },
+};
